53bd082a83101e5d951d8e8e593e1f42bbf1fced
Becky Zhou <beckyzhou@outlook.com>
2013-11-28 15:09:05 -0800

    Added java project

---
+++ Hello.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package p1;
+
+/**
+ * @author a00858577
+ *
+ */
+//********************************************
+//Hello.java
+//
+//Print a Hello, World message.
+//********************************************
+public class Hello
+{
+//-----------------------------------
+//main method -- prints the greeting
+//-----------------------------------
+public static void main (String[] args)
+{
+System.out.println ("Hello, world!");
+}
+}
